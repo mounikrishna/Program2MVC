1e0d736a5183401844e2e367af8f3b474695ffb3
mouni krishna <mounikrishnaa@gmail.com>
2019-02-08 15:38:16 -0600

    First Commit

--- Program2MVC/src/Web/Controllers/HomeController.cs
+++ Program2MVC/src/Web/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace Web.Controllers
             return View();
         }
 
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
